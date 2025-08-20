de4e2fc27226d0d3f19522088265aa0bcdbd323c
elliotjkd <elliotjkd@gmail.com>
2025-08-19 21:48:36 -0500

    added 5 letter word limit to guesses

--- wordlegame.c
+++ wordlegame.c
@@ -1,7 +1,8 @@
 #include <stdio.h> // "Standard Input/Output Header" -- printf(), scanf(), etc. 
 #include <string.h> // Provides String functions -- strlen(), strcpy(), strcat(), etc. 
 
-void feedbackFunction(char secretWord[], char playerGuess[]);
+void feedback_function(char secretWord[], char playerGuess[]);
+void input_buffer(void); 
 
 int main() {
     char secretWord[] = "flake"; // initialize secret word 
@@ -13,13 +14,28 @@ int main() {
 
     for (guessCount = 1; guessCount < 7; guessCount++) { // For loop -- increments guessCount by 1 until the guesses reach 6 
         printf("\nEnter your #%d guess here: ", guessCount); // User guess input 
+
         fgets(playerGuess, sizeof(playerGuess), stdin); // User guess input 
-        playerGuess[strcspn(playerGuess, "\n")] = '\0'; // Removes /n from the end of the player guess to make it 5 characters again 
+
+        if (strchr(playerGuess, '\n') == NULL) {
+            input_buffer();
+        } else {
+            playerGuess[strcspn(playerGuess, "\n")] = '\0'; // Removes /n from the end of the player guess to make it 5 characters again 
+
+        }
+
+        if (strlen(playerGuess) != 5) {
+            printf("\nPlease enter a 5 letter word next time!\n");
+            guessCount--;
+            continue;
+        }
+
         if (strcmp(playerGuess, secretWord) == 0 ) { // If the player guesses the word the game ends 
             printf("\nYou are correct! The wordle was %s", secretWord); 
-            guessCount = 7; // Ends the for loop 
+            guessCount = 7; // Ends the for loop a
+            break;
         } else {
-            feedbackFunction(secretWord, playerGuess); // Gives the user feedback on their guess
+            feedback_function(secretWord, playerGuess); // Gives the user feedback on their guess
             printf("You are incorrect try again."); 
     }
     }
@@ -28,7 +44,7 @@ int main() {
     return 0; 
     }
 
-void feedbackFunction(char secretWord[], char playerGuess[]) {
+void feedback_function(char secretWord[], char playerGuess[]) {
     char feedback[] = "....."; // . Indicates that the letter is not in the word 
     int used[5] = {0}; // Initialize array that keeps track of the letters used in the secret word 
 
@@ -58,4 +74,9 @@ void feedbackFunction(char secretWord[], char playerGuess[]) {
         printf("%c", feedback[i]);
     }
     printf("\n");
+}
+
+void input_buffer(void) {
+     int c;
+    while ( (c = getchar()) != '\n' && c != EOF) {}
 }
